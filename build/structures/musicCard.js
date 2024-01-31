75ea770f493994e3b58241da7529e25f6a80b730
Ramon adipramana hasibullah <129695309+hymoonz@users.noreply.github.com>
2024-02-01 06:30:05 +0700

    musicCard.js

--- build/structures/musicCard.js
+++ build/structures/musicCard.js
@@ -62,6 +62,70 @@ class musicCard {
     }
 
     setProgress(progress) {
+        this.progress = progress;const canvas = require("@napi-rs/canvas");
+const { colorFetch } = require("../functions/colorFetch");
+
+ //canvas.GlobalFonts.registerFromPath(`build/structures/font/circularstd-black.otf`, "circular-std");
+ //canvas.GlobalFonts.registerFromPath(`build/structures/font/notosans-jp-black.ttf`, "noto-sans-jp");
+ //canvas.GlobalFonts.registerFromPath(`build/structures/font/notosans-black.ttf`, "noto-sans");
+ //canvas.GlobalFonts.registerFromPath(`build/structures/font/notoemoji-bold.ttf`, "noto-emoji");
+ //canvas.GlobalFonts.registerFromPath(`build/structures/font/notosans-kr-black.ttf`, "noto-sans-kr");
+ //canvas.GlobalFonts.registerFromPath(`build/structures/font/Space.ttf`, "space");
+
+
+canvas.GlobalFonts.registerFromPath(`node_modules/musicard-quartz/build/structures/font/circularstd-black.otf`, "circular-std");
+canvas.GlobalFonts.registerFromPath(`node_modules/musicard-quartz/build/structures/font/notosans-jp-black.ttf`, "noto-sans-jp");
+canvas.GlobalFonts.registerFromPath(`node_modules/musicard-quartz/build/structures/font/notosans-black.ttf`, "noto-sans");
+canvas.GlobalFonts.registerFromPath(`node_modules/musicard-quartz/build/structures/font/notoemoji-bold.ttf`, "noto-emoji");
+canvas.GlobalFonts.registerFromPath(`node_modules/musicard-quartz/build/structures/font/notosans-kr-black.ttf`, "noto-sans-kr");
+canvas.GlobalFonts.registerFromPath(`node_modules/musicard-quartz/build/structures/font/Chewy-Regular.ttf`, "chewy");
+canvas.GlobalFonts.registerFromPath(`node_modules/musicard-quartz/build/structures/font/Space.ttf`, "space");
+
+
+class musicCard {
+    constructor(options) {
+        this.name = options?.name ?? null;
+        this.author = options?.author ?? null;
+        this.color = options?.color ?? null;
+        this.theme = options?.theme ?? null;
+        this.brightness = options?.brightness ?? null;
+        this.thumbnail = options?.thumbnail ?? null;
+        this.progress = options?.progress ?? null;
+        this.starttime = options?.startTime ?? null;
+        this.endtime = options?.endTime ?? null;
+    }
+
+    setName(name) {
+        this.name = name;
+        return this;
+    }
+
+    setAuthor(author) {
+        this.author = author;
+        return this;
+    }
+
+    setColor(color) {
+        this.color = color;
+        return this;
+    }
+
+    setTheme(theme) {
+        this.theme = theme || 'quartz+';
+        return this;
+    }
+
+    setBrightness(brightness) {
+        this.brightness = brightness;
+        return this;
+    }
+
+    setThumbnail(thumbnail) {
+        this.thumbnail = thumbnail;
+        return this;
+    }
+
+    setProgress(progress) {
         this.progress = progress;
         return this;
     }
@@ -248,8 +312,236 @@ class musicCard {
             return image.toBuffer('image/png');
 
 
+        } if (this.theme == 'onepiece+') {
+            const progressBarWidth = (validatedProgress / 100) * 670;
+            const circleX = progressBarWidth + 60;
+
+            const progressBarCanvas = canvas.createCanvas(670, 25);
+            const progressBarCtx = progressBarCanvas.getContext('2d');
+            const cornerRadius = 10;
+            progressBarCtx.beginPath();
+            progressBarCtx.moveTo(cornerRadius, 0);
+            progressBarCtx.lineTo(670 - cornerRadius, 0);
+            progressBarCtx.arc(670 - cornerRadius, cornerRadius, cornerRadius, 1.5 * Math.PI, 2 * Math.PI);
+            progressBarCtx.lineTo(670, 25 - cornerRadius);
+            progressBarCtx.arc(670 - cornerRadius, 25 - cornerRadius, cornerRadius, 0, 0.5 * Math.PI);
+            progressBarCtx.lineTo(cornerRadius, 25);
+            progressBarCtx.arc(cornerRadius, 25 - cornerRadius, cornerRadius, 0.5 * Math.PI, Math.PI);
+            progressBarCtx.lineTo(0, cornerRadius);
+            progressBarCtx.arc(cornerRadius, cornerRadius, cornerRadius, Math.PI, 1.5 * Math.PI);
+            progressBarCtx.closePath();
+            progressBarCtx.fillStyle = '#ababab';
+            progressBarCtx.fill();
+            progressBarCtx.beginPath();
+            progressBarCtx.moveTo(cornerRadius, 0);
+            progressBarCtx.lineTo(progressBarWidth - cornerRadius, 0);
+            progressBarCtx.arc(progressBarWidth - cornerRadius, cornerRadius, cornerRadius, 1.5 * Math.PI, 2 * Math.PI);
+            progressBarCtx.lineTo(progressBarWidth, 25);
+            progressBarCtx.lineTo(cornerRadius, 25);
+            progressBarCtx.arc(cornerRadius, 25 - cornerRadius, cornerRadius, 0.5 * Math.PI, Math.PI);
+            progressBarCtx.lineTo(0, cornerRadius);
+            progressBarCtx.arc(cornerRadius, cornerRadius, cornerRadius, Math.PI, 1.5 * Math.PI);
+            progressBarCtx.closePath();
+            progressBarCtx.fillStyle = `#${validatedColor}`;
+            progressBarCtx.fill();
+
+            const circleCanvas = canvas.createCanvas(1000, 1000);
+            const circleCtx = circleCanvas.getContext('2d');
+
+            const circleRadius = 20;
+            const circleY = 97;
+
+            circleCtx.beginPath();
+            circleCtx.arc(circleX, circleY, circleRadius, 0, 2 * Math.PI);
+            circleCtx.fillStyle = `#ff0000`;
+            circleCtx.fill();
+
+            const background = await canvas.loadImage(`https://i.imgur.com/cYRzWfL.png`);
+
+            const thumbnailCanvas = canvas.createCanvas(564, 564);
+            const thumbnailCtx = thumbnailCanvas.getContext('2d');
+
+            let thumbnailImage;
+
+           
+            try {
+                thumbnailImage = await canvas.loadImage(this.thumbnail, {
+                    requestOptions: {
+                        headers: {
+                            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36',
+                        }
+                    }
+                });
+            } catch (error) {
+            
+                throw new Error(`Failed to load Thumnail Image\n The Image URl is invalid or the image is not accessible\n${error}`);
+            }
+
+            const thumbnailSize = Math.min(thumbnailImage.width, thumbnailImage.height);
+            const thumbnailX = (thumbnailImage.width - thumbnailSize) / 2;
+            const thumbnailY = (thumbnailImage.height - thumbnailSize) / 2;
+
+            const cornerRadius2 = 45;
+
+            thumbnailCtx.beginPath();
+            thumbnailCtx.moveTo(0 + cornerRadius2, 0);
+            thumbnailCtx.arcTo(thumbnailCanvas.width, 0, thumbnailCanvas.width, thumbnailCanvas.height, cornerRadius2);
+            thumbnailCtx.arcTo(thumbnailCanvas.width, thumbnailCanvas.height, 0, thumbnailCanvas.height, cornerRadius2);
+            thumbnailCtx.arcTo(0, thumbnailCanvas.height, 0, 0, cornerRadius2);
+            thumbnailCtx.arcTo(0, 0, thumbnailCanvas.width, 0, cornerRadius2);
+            thumbnailCtx.closePath();
+            thumbnailCtx.clip();
+
+            thumbnailCtx.drawImage(thumbnailImage, thumbnailX, thumbnailY, thumbnailSize, thumbnailSize, 0, 0, thumbnailCanvas.width, thumbnailCanvas.height);
+
+            const image = canvas.createCanvas(1280, 450);
+            const ctx = image.getContext('2d');
+
+            ctx.drawImage(background, 0, 0, 1280, 450);
+
+            ctx.fillStyle = `#fff`;
+            ctx.font = `50px circular-std`;
+            ctx.fillText(this.name, 250, 120);
+
+            ctx.fillStyle = '#fff';
+            ctx.font = `25px circular-std`;
+            ctx.fillText(this.author, 255, 180);
+
+            ctx.fillStyle = '#fff';
+            ctx.font = '30px circular-std';
+            ctx.fillText(validatedStartTime, 70, 315);
+
+            ctx.fillStyle = '#fff';
+            ctx.font = '30px circular-std';
+            ctx.fillText(validatedEndTime, 660, 315);
+
+            ctx.drawImage(thumbnailCanvas, 45, 35, 180, 180);
+            ctx.drawImage(progressBarCanvas, 70, 250, 670, 25);
+            ctx.drawImage(circleCanvas, 10, 165, 1000, 1000);
+
+            return image.toBuffer('image/png');
+        }  else if (this.theme === "vector+") {
+
+            const progressBarWidth = (validatedProgress / 100) * 670;
+            const circleX = progressBarWidth + 60;
+
+            const progressBarCanvas = canvas.createCanvas(670, 25);
+            const progressBarCtx = progressBarCanvas.getContext('2d');
+            const cornerRadius = 10;
+            progressBarCtx.beginPath();
+            progressBarCtx.moveTo(cornerRadius, 0);
+            progressBarCtx.lineTo(670 - cornerRadius, 0);
+            progressBarCtx.arc(670 - cornerRadius, cornerRadius, cornerRadius, 1.5 * Math.PI, 2 * Math.PI);
+            progressBarCtx.lineTo(670, 25 - cornerRadius);
+            progressBarCtx.arc(670 - cornerRadius, 25 - cornerRadius, cornerRadius, 0, 0.5 * Math.PI);
+            progressBarCtx.lineTo(cornerRadius, 25);
+            progressBarCtx.arc(cornerRadius, 25 - cornerRadius, cornerRadius, 0.5 * Math.PI, Math.PI);
+            progressBarCtx.lineTo(0, cornerRadius);
+            progressBarCtx.arc(cornerRadius, cornerRadius, cornerRadius, Math.PI, 1.5 * Math.PI);
+            progressBarCtx.closePath();
+            progressBarCtx.fillStyle = '#ababab';
+            progressBarCtx.fill();
+            progressBarCtx.beginPath();
+            progressBarCtx.moveTo(cornerRadius, 0);
+            progressBarCtx.lineTo(progressBarWidth - cornerRadius, 0);
+            progressBarCtx.arc(progressBarWidth - cornerRadius, cornerRadius, cornerRadius, 1.5 * Math.PI, 2 * Math.PI);
+            progressBarCtx.lineTo(progressBarWidth, 25);
+            progressBarCtx.lineTo(cornerRadius, 25);
+            progressBarCtx.arc(cornerRadius, 25 - cornerRadius, cornerRadius, 0.5 * Math.PI, Math.PI);
+            progressBarCtx.lineTo(0, cornerRadius);
+            progressBarCtx.arc(cornerRadius, cornerRadius, cornerRadius, Math.PI, 1.5 * Math.PI);
+            progressBarCtx.closePath();
+            progressBarCtx.fillStyle = `#${validatedColor}`;
+            progressBarCtx.fill();
+
+            const circleCanvas = canvas.createCanvas(1000, 1000);
+            const circleCtx = circleCanvas.getContext('2d');
+
+            const circleRadius = 20;
+            const circleY = 97;
+
+            circleCtx.beginPath();
+            circleCtx.arc(circleX, circleY, circleRadius, 0, 2 * Math.PI);
+            circleCtx.fillStyle = `#${validatedColor}`;
+            circleCtx.fill();
+
+            const background = await canvas.loadImage(`https://i.imgur.com/Kgzsiy9.png`);
+
+            const thumbnailCanvas = canvas.createCanvas(650, 650);
+            const thumbnailCtx = thumbnailCanvas.getContext('2d');
+
+
+            let thumbnailImage;
+
+           
+            try {
+                thumbnailImage = await canvas.loadImage(this.thumbnail, {
+                    requestOptions: {
+                        headers: {
+                            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36',
+                        }
+                    }
+                });
+            } catch (error) {
+            
+                throw new Error(`Failed to load Thumnail Image\n The Image URl is invalid or the image is not accessible\n${error}`);
+            }
+            const thumbnailSize = Math.min(thumbnailImage.width, thumbnailImage.height);
+            const thumbnailX = (thumbnailImage.width - thumbnailSize) / 2;
+            const thumbnailY = (thumbnailImage.height - thumbnailSize) / 2;
+
+            const cornerRadius2 = 45;
+
+            thumbnailCtx.beginPath();
+            thumbnailCtx.moveTo(0 + cornerRadius2, 0);
+            thumbnailCtx.arcTo(thumbnailCanvas.width, 0, thumbnailCanvas.width, thumbnailCanvas.height, cornerRadius2);
+            thumbnailCtx.arcTo(thumbnailCanvas.width, thumbnailCanvas.height, 0, thumbnailCanvas.height, cornerRadius2);
+            thumbnailCtx.arcTo(0, thumbnailCanvas.height, 0, 0, cornerRadius2);
+            thumbnailCtx.arcTo(0, 0, thumbnailCanvas.width, 0, cornerRadius2);
+            thumbnailCtx.closePath();
+            thumbnailCtx.clip();
+
+            thumbnailCtx.drawImage(thumbnailImage, thumbnailX, thumbnailY, thumbnailSize, thumbnailSize, 0, 0, thumbnailCanvas.width, thumbnailCanvas.height);
+
+            const image = canvas.createCanvas(1280, 450);
+            const ctx = image.getContext('2d');
+
+            // Draw the background
+            ctx.drawImage(background, 0, 0, 1280, 450);
+
+            // Apply fade effect between background and upper parts
+            const gradient = ctx.createLinearGradient(0, 0, 0, 450);
+            gradient.addColorStop(0, 'rgba(0,0,0,0.7)'); // Fully transparent
+            gradient.addColorStop(0.5, 'rgba(0,0,0,0.7)'); // 50% transparent
+            gradient.addColorStop(1, 'rgba(0,0,0,0.7)'); // Fully opaque
+
+            ctx.fillStyle = gradient;
+            ctx.fillRect(0, 0, 1280, 450);
+            ctx.fillStyle = `#${validatedColor}`;
+            ctx.font = `110px space`;
+            ctx.fillText(this.name, 490, 200); 
+
+            ctx.fillStyle = '#b8b8b8';
+            ctx.font = `55px space`;
+            ctx.fillText(this.author, 510, 260); 
+
+            ctx.fillStyle = '#fff';
+            ctx.font = '30px space';
+            ctx.fillText(validatedStartTime, 510, 410); 
+
+            ctx.fillStyle = '#fff';
+            ctx.font = '30px space';
+            ctx.fillText(validatedEndTime, 1100, 410);
+
+           ctx.drawImage(thumbnailCanvas, 70, 50, 350, 350);
+            ctx.drawImage(progressBarCanvas, 510, 340, 670, 25); 
+            ctx.drawImage(circleCanvas, 445, 253, 1000, 1000);
+
+            return image.toBuffer('image/png');
+
+
         } else {
-            throw new Error('Invalid theme parameter, must be  one of "quartz+"');
+            throw new Error('Invalid theme parameter, must be  one of "quartz+" "onepiece+" "vector+"');
         }
     }
 }
